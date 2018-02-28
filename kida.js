76f8f837cc8aa14d6dfb16716fac3465d7c27b6c
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-02-28 15:58:41 +0530

    kida

--- kida.js
+++ kida.js
@@ -5,7 +5,6 @@ jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.3/rythm.mi
     jQuery("div").addClass('rythm-bass');
     var rythm = new Rythm();
 	rythm.setMusic("172.132.45.39/vaptshika1/sample.mp3");
-	//rythm.crossorigin = 'anonymous';
 	rythm.start();
 });
 
