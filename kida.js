2f742bc906e25f635cff778173c23b7fda7f9322
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-02-28 16:49:23 +0530

    kida

--- kida.js
+++ kida.js
@@ -1,7 +1,7 @@
 jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.3/rythm.min.js', function() {
     jQuery("body").addClass('body');
     var rythm = new Rythm();
-    rythm.addRythm('body', 'twist', 0, 10, { min: 0, max: 270 })
+    rythm.addRythm('body', 'twist', 0, 0, { min: 0, max: 360 })
     //rythm.addRythm('thumbnail', 'twist', 0, 10, { min: 10, max: 270 })
     //rythm.addRythm('tjlmspin', 'twist', 0, 10, { min: 10, max: 270 })
 	rythm.setMusic("sample.mp3");
