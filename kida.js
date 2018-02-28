e8a8d34e3ec592b1a93fca98145b565c17bcf983
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-02-28 15:45:19 +0530

    kida

--- kida.js
+++ kida.js
@@ -1,13 +1,9 @@
-// Import Rythm Js
-var imported1 = document.createElement('script');
-imported1.src = 'https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.3/rythm.min.js';
-document.head.appendChild(imported1);
+jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.3/rythm.min.js', function() {
+    alert('Load was performed.');
+    jQuery("div").addClass('rythm-bass');
+    var rythm = new Rythm();
+	rythm.setMusic("https://rawgit.com/Okazari/Rythm.js/master/samples/rythmC.mp3");
+	rythm.start();
+});
 
-// Append rythm-bass class to each div
-jQuery("div").addClass('rythm-bass');
 
-// Intialize Dancing
-
-var rythm = new Rythm();
-rythm.setMusic("https://rawgit.com/Okazari/Rythm.js/master/samples/rythmC.mp3");
-rythm.start();
