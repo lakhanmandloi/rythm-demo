4640355f4742719dfc845f126164e2eda4c639ec
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-02-28 16:00:09 +0530

    kida

--- kida.js
+++ kida.js
@@ -2,7 +2,7 @@ jQuery('head').append('<header name = "Access-Control-Allow-Origin" value = "*" 
 
 jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.3/rythm.min.js', function() {
     alert('Load was performed.');
-    jQuery("div").addClass('rythm-bass');
+    jQuery(".row").addClass('rythm-bass');
     var rythm = new Rythm();
 	rythm.setMusic("172.132.45.39/vaptshika1/sample.mp3");
 	rythm.start();
