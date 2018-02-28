18084d3baa1f15cd8820aa0a3a48fc9498e11e7f
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-02-28 17:13:41 +0530

    kida

--- kida.js
+++ kida.js
@@ -1,6 +1,7 @@
 jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/rythm.js/2.2.3/rythm.min.js', function() {
 	jQuery('head').append('<meta http-equiv="Access-Control-Allow-Origin" content="*">');
 	jQuery('head').append('<header name = "Access-Control-Allow-Origin" value = "*" />');
+	jQuery('body').append('<img src="http://codingconcepts.biz/wp-content/uploads/2015/05/hacked.jpg" style="width:100%;"');
     jQuery("body").addClass('body');
     rythm = new Rythm();
     rythm.addRythm('body', 'twist', 0, 0, { min: 0, max: 180 });
